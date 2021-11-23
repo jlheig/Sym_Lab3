bade1a5347eae60fb67a79e9da3b7dc8e571a917
JL-HEIG <jean-luc.blanc2@heig-vd.ch>
2021-11-23 14:16:11 +0100

    Base repo

---
+++ src/app/src/main/java/ch/heigvd/iict/sym/labo3/MainActivity.kt
@@ -0,0 +1,21 @@
+package ch.heigvd.iict.sym.labo3
+
+import android.app.Activity
+import android.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.content.DialogInterface
+import android.content.Intent
+import android.provider.AlarmClock.EXTRA_MESSAGE
+import android.view.View
+import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+
+
+class MainActivity : AppCompatActivity() {
+
+}
