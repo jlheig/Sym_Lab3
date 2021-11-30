9fd7b544eb7eac7148b91126eb303c0f6dc898fb
PaulReeve717 <paulreeve717@gmail.com>
2021-11-30 14:03:31 +0100

    Link main layout to main activity

--- src/app/src/main/java/ch/heigvd/iict/sym/labo3/MainActivity.kt
+++ src/app/src/main/java/ch/heigvd/iict/sym/labo3/MainActivity.kt
@@ -9,6 +9,7 @@ import android.widget.Button
 import android.widget.EditText
 import android.content.DialogInterface
 import android.content.Intent
+import android.os.PersistableBundle
 import android.provider.AlarmClock.EXTRA_MESSAGE
 import android.view.View
 import android.widget.Toast
@@ -18,4 +19,16 @@ import androidx.activity.result.contract.ActivityResultContracts
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var nfcBtn : Button
+    private lateinit var barcodeBtn : Button
+    private lateinit var ibeaconBtn : Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        nfcBtn = findViewById(R.id.main_nfcButton)
+        barcodeBtn   = findViewById(R.id.main_codeBarreButton)
+        ibeaconBtn   = findViewById(R.id.main_iBeaconButton)
+    }
 }
