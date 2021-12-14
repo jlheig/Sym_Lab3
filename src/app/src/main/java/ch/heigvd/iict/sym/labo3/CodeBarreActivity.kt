ee6df23099321c5a21e5cf9e36bbcf038f174f51
JL-HEIG <jean-luc.blanc2@heig-vd.ch>
2021-12-14 18:28:00 +0100

    testing on phone

---
+++ src/app/src/main/java/ch/heigvd/iict/sym/labo3/CodeBarreActivity.kt
@@ -0,0 +1,76 @@
+package ch.heigvd.iict.sym.labo3
+
+import android.graphics.Color
+import android.media.Image
+import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.ResultPoint
+import com.google.zxing.client.android.BeepManager
+import com.journeyapps.barcodescanner.BarcodeCallback
+import com.journeyapps.barcodescanner.BarcodeResult
+import com.journeyapps.barcodescanner.DecoratedBarcodeView
+import com.journeyapps.barcodescanner.DefaultDecoderFactory
+import java.util.*
+
+/**
+ * This sample performs continuous scanning, displaying the barcode and source image whenever
+ * a barcode is scanned.
+ */
+class CodeBarreActivity : AppCompatActivity() {
+    private lateinit var barcodeView: DecoratedBarcodeView
+    private lateinit var beepManager: BeepManager
+    private lateinit var lastText: TextView
+    private lateinit var imageView : ImageView
+
+    private val callback: BarcodeCallback = object : BarcodeCallback {
+        override fun barcodeResult(result: BarcodeResult) {
+            if (result.text == null || result.text == lastText.text) {
+                // Prevent duplicate scans
+                return
+            }
+            lastText.text = result.text
+            barcodeView.setStatusText(result.text)
+            beepManager.playBeepSoundAndVibrate()
+
+            //Added preview of scanned barcode
+            imageView.setImageBitmap(result.getBitmapWithResultPoints(Color.YELLOW))
+        }
+
+        override fun possibleResultPoints(resultPoints: List<ResultPoint>) {}
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_code_barre)
+
+        barcodeView = findViewById(R.id.barcode_scanner)
+        val formats: Collection<BarcodeFormat> =
+            Arrays.asList(BarcodeFormat.QR_CODE, BarcodeFormat.CODE_39)
+        barcodeView.getBarcodeView().decoderFactory = DefaultDecoderFactory(formats)
+        barcodeView.initializeFromIntent(intent)
+        barcodeView.decodeContinuous(callback)
+
+        beepManager = BeepManager(this)
+        imageView = findViewById(R.id.barcode_img)
+        lastText = findViewById(R.id.barcode_result_content)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        barcodeView.resume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        barcodeView.pause()
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        return barcodeView.onKeyDown(keyCode, event) || super.onKeyDown(keyCode, event)
+    }
+}
--- src/app/src/main/java/ch/heigvd/iict/sym/labo3/MainActivity.kt
+++ src/app/src/main/java/ch/heigvd/iict/sym/labo3/MainActivity.kt
@@ -9,12 +9,16 @@ import android.widget.Button
 import android.widget.EditText
 import android.content.DialogInterface
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.PersistableBundle
 import android.provider.AlarmClock.EXTRA_MESSAGE
 import android.view.View
 import android.widget.Toast
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import android.Manifest
 
 
 class MainActivity : AppCompatActivity() {
@@ -23,6 +27,10 @@ class MainActivity : AppCompatActivity() {
     private lateinit var barcodeBtn : Button
     private lateinit var ibeaconBtn : Button
 
+    companion object {
+        private const val CAMERA_PERMISSION = 100
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -30,5 +38,34 @@ class MainActivity : AppCompatActivity() {
         nfcBtn = findViewById(R.id.main_nfcButton)
         barcodeBtn   = findViewById(R.id.main_codeBarreButton)
         ibeaconBtn   = findViewById(R.id.main_iBeaconButton)
+
+        barcodeBtn.setOnClickListener {
+            checkPermissions(Manifest.permission.CAMERA, CAMERA_PERMISSION)
+        }
+    }
+
+
+    //Next two functions are from GeeksForGeeks, their tutorial was really useful
+
+    private fun checkPermissions(permission: String, permissionCode: Int){
+        if(ContextCompat.checkSelfPermission(this@MainActivity, permission) == PackageManager.PERMISSION_DENIED){
+            ActivityCompat.requestPermissions(this@MainActivity, arrayOf(permission), permissionCode);
+        }
+        else{
+            startActivity(Intent(this, CodeBarreActivity::class.java))
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == CAMERA_PERMISSION){
+            if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_DENIED){
+                startActivity(Intent(this, CodeBarreActivity::class.java))
+            }
+        }
     }
 }
